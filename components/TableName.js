e2d804b57621db04115bc7429ad77c0c7ffe2447
wagmit <hello@wagmi.se>
2024-10-12 15:52:19 +0800

    Unoptimize logo

--- components/TableName.js
+++ components/TableName.js
@@ -10,16 +10,17 @@ export default function TableName({ name, logo, linkUrl }) {
         <a href={linkUrl} target="_blank" rel="noopener noreferrer" className="flex items-center gap-3">
           {/* Left Column (Logo) */}
           {/* Commenting out the logo section */}
-          {/* <div className="flex-none w-11 h-11 relative">
+           <div className="flex-none w-11 h-11 relative">
             <div className="absolute inset-0 border-2 border-black rounded-full">
               <Image
                 src={logo || '/path/to/default/image.png'}
                 alt="Logo"
                 layout="fill" // Use layout fill for responsive image size within the container
                 objectFit="cover" // Cover to ensure the image covers the available space without stretching
+                unoptimized // Add this prop to disable optimization
               />
             </div>
-          </div> */}
+          </div>
           {/* Right Column (Heading) */}
           <div className="flex-grow overflow-hidden"> {/* Changed to overflow-hidden to prevent text spillover */}
             <h3>{name}</h3>
--- components/TableNameMobile.js
+++ components/TableNameMobile.js
@@ -9,7 +9,6 @@ export default function TableNameMobile({ name, logo, linkUrl }) {
         {/* Wrap both Image and Name within the anchor tag */}
         <a href={linkUrl} target="_blank" rel="noopener noreferrer" className="flex items-center gap-3">
           {/* Left Column (Logo) */}
-          {/* Commenting out the logo section
           <div className="flex-none w-11 h-11 relative">
             <div className="absolute inset-0 border-2 border-black rounded-full">
               <Image
@@ -17,10 +16,11 @@ export default function TableNameMobile({ name, logo, linkUrl }) {
                 alt="Logo"
                 layout="fill" // Use layout fill for responsive image size within the container
                 objectFit="cover" // Cover to ensure the image covers the available space without stretching
+                unoptimized // Add this prop to disable image optimization
               />
             </div>
           </div>
-          */}
+        
           {/* Right Column (Heading) */}
           <div className="flex-grow overflow-hidden"> {/* Changed to overflow-hidden to prevent text spillover */}
             <h3>{name}</h3>
