2f795655c9bd77f55122f5beaf2a7903b4062791
wagmit <hello@wagmi.se>
2025-01-19 08:03:46 +0100

    X and discord

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,55 @@
 // components/Footer.js
 
+import React from "react";
+import Image from "next/image";
+import XLogo from "../src/img/icon-x-21.png"; // Make sure this path matches your image location
+import DiscordLogo from "../src/img/icon-discord.png"; // Make sure this path matches your image location
+
 const Footer = () => {
-    return (
-      <footer className="mt-8">
-                <hr className="w-full p-2 border-t-2 border-black" /> {/* Add this line */}
-        <div className=" text-black  text-center items-center justify-center p-4">
-        <p>theincognita.com</p>
-        <br />
+  return (
+    <footer className="mt-8">
+      <hr className="w-full p-2 border-t-2 border-black" />{" "}
+      {/* Add this line */}
+      <div className="text-black text-center items-center justify-center p-4">
         <p>Made with ❤️ by the Incognita community</p>
-        <p>Help us at <a href="https://github.com/wagmiteth/theincognita.com"target="_blank" rel="noopener noreferrer">Github</a> </p>
+        <p>
+          Help us at{" "}
+          <a
+            href="https://github.com/wagmiteth/theincognita.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </p>
+        <br />
+        <p>theincognita.com</p>
+        <div className="flex justify-center gap-4 my-4">
+          <a
+            href="https://x.com/incognitaeth"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-transform hover:scale-110"
+          >
+            <Image src={XLogo} alt="X (Twitter) Logo" width={20} height={20} />
+          </a>
+          <a
+            href="https://discord.gg/EyvF2fEqWT"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-transform hover:scale-110"
+          >
+            <Image
+              src={DiscordLogo}
+              alt="Discord Logo"
+              width={20}
+              height={20}
+            />
+          </a>
         </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
