448a48d54c3a22dab72cefe0707bb0695fca81f0
Cookie <evita@stenqvist.co>
2025-04-30 11:09:30 +0200

    Fixed margin on logo/title on mobile.

--- components/TableNameMobile.js
+++ components/TableNameMobile.js
@@ -1,18 +1,23 @@
 // components/TableNameMobile.js
 import React from "react";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function TableNameMobile({ name, logo, linkUrl }) {
   return (
-    <div className="container mx-auto">
+    <div className="container">
       <div className="flex items-center gap-3 h-[72px]">
         {/* Wrap both Image and Name within the anchor tag */}
-        <a href={linkUrl} target="_blank" rel="noopener noreferrer" className="flex items-center gap-3">
+        <a
+          href={linkUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-3"
+        >
           {/* Left Column (Logo) */}
           <div className="flex-none w-11 h-11 relative">
             <div className="absolute inset-0 border-2 border-black rounded-full">
               <Image
-                src={logo || '/path/to/default/image.png'}
+                src={logo || "/path/to/default/image.png"}
                 alt="Logo"
                 layout="fill" // Use layout fill for responsive image size within the container
                 objectFit="cover" // Cover to ensure the image covers the available space without stretching
@@ -20,9 +25,11 @@ export default function TableNameMobile({ name, logo, linkUrl }) {
               />
             </div>
           </div>
-        
+
           {/* Right Column (Heading) */}
-          <div className="flex-grow overflow-hidden"> {/* Changed to overflow-hidden to prevent text spillover */}
+          <div className="flex-grow overflow-hidden">
+            {" "}
+            {/* Changed to overflow-hidden to prevent text spillover */}
             <h3>{name}</h3>
           </div>
         </a>
